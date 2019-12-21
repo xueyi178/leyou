cb42773a239dbbd215bee742060bb7b21091e909
xueyi178 <843483169@qq.com>
2019-12-21 14:38:22 +0800

    添加注解

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -1,9 +1,11 @@
 package com.leyou;
 
-import tk.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @ProjectName leyou
@@ -18,7 +20,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @MapperScan("com.leyou.item.mapper")
 public class LyItemApplication {
 
+    private static Logger log = LoggerFactory.getLogger(LyItemApplication.class);
+
     public static void main(String[] args) {
+        log.info("商品服务提交成功");
         SpringApplication.run(LyItemApplication.class);
     }
 }
