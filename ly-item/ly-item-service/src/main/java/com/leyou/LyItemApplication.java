2ad50136966d7837efe5e4df44070009c778fb94
xueyi178 <843483169@qq.com>
2019-12-21 14:59:05 +0800

    添加注解

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -24,7 +24,7 @@ public class LyItemApplication {
 
     public static void main(String[] args) {
         log.info("商品服务提交成功");
-        log.info("薛毅好刷会反反复复付");
+        log.info("masterfenzh ");
         SpringApplication.run(LyItemApplication.class);
     }
 }
