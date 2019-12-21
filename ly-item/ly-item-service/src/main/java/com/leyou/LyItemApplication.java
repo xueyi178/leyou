a4afc7d85018780099ba5393a63bee7f4ed575c4
xueyi178 <843483169@qq.com>
2019-12-21 15:06:53 +0800

    修改dev

--- ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -25,6 +25,7 @@ public class LyItemApplication {
     public static void main(String[] args) {
         log.info("商品服务提交成功");
         log.info("dev分支少时诵诗书所 ");
+        log.info("dev分支少时诵诗书所少时诵诗书所所所所所所所所所 ");
         SpringApplication.run(LyItemApplication.class);
     }
 }
