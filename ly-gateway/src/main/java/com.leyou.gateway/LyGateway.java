234dfad1292abe7d7a485d3726f61b289956e24d
xueyi178 <843483169@qq.com>
2020-02-05 13:28:24 +0800

    添加注解

--- ly-gateway/src/main/java/com.leyou.gateway/LyGateway.java
+++ ly-gateway/src/main/java/com.leyou.gateway/LyGateway.java
@@ -1,5 +1,7 @@
 package com.leyou.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -18,7 +20,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 public class LyGateway {
 
+    private static Logger log = LoggerFactory.getLogger(LyGateway.class);
+
     public static void main(String[] args) {
         SpringApplication.run(LyGateway.class);
+        log.info("LyGateway网关启动成功");
     }
 }
